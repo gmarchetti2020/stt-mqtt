32df63244a2f065d27d71680c80bf06e83eefb70
gmarchetti2020 <59297378+gmarchetti2020@users.noreply.github.com>
2023-02-17 12:26:32 -0800

    prefer offline speech recognizer

--- app/src/main/java/com/dimingo/voicemqtt/MainActivity.java
+++ app/src/main/java/com/dimingo/voicemqtt/MainActivity.java
@@ -142,6 +142,7 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
 
 		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+		intent.putExtra(RecognizerIntent.EXTRA_PREFER_OFFLINE,true);
 		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
 
 		if (intent.resolveActivity(getPackageManager()) != null) {
